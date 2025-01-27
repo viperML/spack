viperML/spack

commit d8c80747624e32ade173de081473782e3fb7eebe

bootstrap: add clingo 3.13 binaries and more (#47126)

lib/spack/spack/bootstrap/core.py
@@ -37,6 +37,7 @@ from llnl.util.lang import GroupedExceptionHandler
 import spack.binary_distribution
 import spack.config
 import spack.detection
+import spack.mirror
 import spack.platforms
 import spack.spec
 import spack.store
@@ -91,12 +92,7 @@ class Bootstrapper:
         self.metadata_dir = spack.util.path.canonicalize_path(conf["metadata"])
 
         # Promote (relative) paths to file urls
-        url = conf["info"]["url"]
-        if spack.util.url.is_path_instead_of_url(url):
-            if not os.path.isabs(url):
-                url = os.path.join(self.metadata_dir, url)
-            url = spack.util.url.path_to_file_url(url)
-        self.url = url
+        self.url = spack.mirror.Mirror(conf["info"]["url"]).fetch_url
 
     @property
     def mirror_scope(self) -> spack.config.InternalConfigScope:

lib/spack/spack/test/cmd/bootstrap.py
@@ -170,7 +170,7 @@ def test_remove_and_add_a_source(mutable_config):
     assert not sources
 
     # Add it back and check we restored the initial state
-    _bootstrap("add", "github-actions", "$spack/share/spack/bootstrap/github-actions-v0.5")
+    _bootstrap("add", "github-actions", "$spack/share/spack/bootstrap/github-actions-v0.6")
     sources = spack.bootstrap.core.bootstrapping_sources()
     assert len(sources) == 1
 

.github/workflows/bin/bootstrap-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 source share/spack/setup-env.sh
-$PYTHON bin/spack bootstrap disable github-actions-v0.4
+$PYTHON bin/spack bootstrap disable github-actions-v0.5
 $PYTHON bin/spack bootstrap disable spack-install
 $PYTHON bin/spack $SPACK_FLAGS solve zlib
 tree $BOOTSTRAP/store

.github/workflows/bootstrap.yml
@@ -43,8 +43,8 @@ jobs:
       - name: Bootstrap clingo
         run: |
           source share/spack/setup-env.sh
+          spack bootstrap disable github-actions-v0.6
           spack bootstrap disable github-actions-v0.5
-          spack bootstrap disable github-actions-v0.4
           spack external find cmake bison
           spack -d solve zlib
           tree ~/.spack/bootstrap/store/
@@ -69,8 +69,8 @@ jobs:
       - name: Bootstrap clingo
         run: |
           source share/spack/setup-env.sh
+          spack bootstrap disable github-actions-v0.6
           spack bootstrap disable github-actions-v0.5
-          spack bootstrap disable github-actions-v0.4
           spack external find --not-buildable cmake bison
           spack -d solve zlib
           tree $HOME/.spack/bootstrap/store/
@@ -97,8 +97,8 @@ jobs:
         run: |
           source share/spack/setup-env.sh
           spack solve zlib
+          spack bootstrap disable github-actions-v0.6
           spack bootstrap disable github-actions-v0.5
-          spack bootstrap disable github-actions-v0.4
           spack -d gpg list
           tree ~/.spack/bootstrap/store/
 
@@ -130,15 +130,16 @@ jobs:
             3.10
             3.11
             3.12
+            3.13
       - name: Set bootstrap sources
         run: |
           source share/spack/setup-env.sh
-          spack bootstrap disable github-actions-v0.4
+          spack bootstrap disable github-actions-v0.5
           spack bootstrap disable spack-install
       - name: Bootstrap clingo
         run: |
           set -e
-          for ver in '3.8' '3.9' '3.10' '3.11' '3.12' ; do
+          for ver in '3.8' '3.9' '3.10' '3.11' '3.12' '3.13'; do
             not_found=1
             ver_dir="$(find $RUNNER_TOOL_CACHE/Python -wholename "*/${ver}.*/*/bin" | grep . || true)"
             if [[ -d "$ver_dir" ]] ; then
@@ -185,8 +186,8 @@ jobs:
       - name: Bootstrap clingo
         run: |
           ./share/spack/setup-env.ps1
+          spack bootstrap disable github-actions-v0.6
           spack bootstrap disable github-actions-v0.5
-          spack bootstrap disable github-actions-v0.4
           spack external find --not-buildable cmake bison
           spack -d solve zlib
           ./share/spack/qa/validate_last_exit.ps1

.github/workflows/unit_tests.yaml
@@ -170,8 +170,8 @@ jobs:
       run: |
         . share/spack/setup-env.sh
         spack bootstrap disable spack-install
-        spack bootstrap disable github-actions-v0.4
         spack bootstrap disable github-actions-v0.5
+        spack bootstrap disable github-actions-v0.6
         spack bootstrap status
         spack solve zlib
         spack unit-test --verbose --cov --cov-config=pyproject.toml --cov-report=xml:coverage.xml lib/spack/spack/test/concretize.py

etc/spack/defaults/bootstrap.yaml
@@ -9,15 +9,15 @@ bootstrap:
   # may not be able to bootstrap all the software that Spack needs,
   # depending on its type.
   sources:
-  - name: 'github-actions-v0.5'
+  - name: github-actions-v0.6
+    metadata: $spack/share/spack/bootstrap/github-actions-v0.6
+  - name: github-actions-v0.5
     metadata: $spack/share/spack/bootstrap/github-actions-v0.5
-  - name: 'github-actions-v0.4'
-    metadata: $spack/share/spack/bootstrap/github-actions-v0.4
-  - name: 'spack-install'
+  - name: spack-install
     metadata: $spack/share/spack/bootstrap/spack-install
   trusted:
     # By default we trust bootstrapping from sources and from binaries
     # produced on Github via the workflow
+    github-actions-v0.6: true
     github-actions-v0.5: true
-    github-actions-v0.4: true
     spack-install: true

lib/spack/spack/test/data/config/bootstrap.yaml
@@ -1,5 +1,5 @@
 bootstrap:
   sources:
   - name: 'github-actions'
-    metadata: $spack/share/spack/bootstrap/github-actions-v0.5
+    metadata: $spack/share/spack/bootstrap/github-actions-v0.6
   trusted: {}

share/spack/bootstrap/github-actions-v0.6/metadata.yaml
@@ -1,8 +1,8 @@
 type: buildcache
 description: |
-  Buildcache generated from a public workflow using Github Actions.
+  Buildcache generated from a public workflow using GitHub Actions hosted on GitHub Packages.
   The sha256 checksum of binaries is checked before installation.
 info:
-  url: https://mirror.spack.io/bootstrap/github-actions/v0.4
+  url: oci://ghcr.io/spack/bootstrap-buildcache-v1
   homepage: https://github.com/spack/spack-bootstrap-mirrors
   releases: https://github.com/spack/spack-bootstrap-mirrors/releases

commit 22ba366e85c1a541200b6fd33e114398f346f2d0

Fix creating a bootstrap mirrors (#48252)
Regressed in #47126 

Spack was not interpreting mirrors using relative path with respect to the
metadata directory.

---------

Co-authored-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

lib/spack/spack/bootstrap/core.py
@@ -46,6 +46,7 @@ import spack.user_environment
 import spack.util.executable
 import spack.util.path
 import spack.util.spack_yaml
+import spack.util.url
 import spack.version
 from spack.installer import PackageInstaller
 
@@ -97,8 +98,12 @@ class Bootstrapper:
         self.name = conf["name"]
         self.metadata_dir = spack.util.path.canonicalize_path(conf["metadata"])
 
-        # Promote (relative) paths to file urls
-        self.url = spack.mirrors.mirror.Mirror(conf["info"]["url"]).fetch_url
+        # Check for relative paths, and turn them into absolute paths
+        # root is the metadata_dir
+        maybe_url = conf["info"]["url"]
+        if spack.util.url.is_path_instead_of_url(maybe_url) and not os.path.isabs(maybe_url):
+            maybe_url = os.path.join(self.metadata_dir, maybe_url)
+        self.url = spack.mirrors.mirror.Mirror(maybe_url).fetch_url
 
     @property
     def mirror_scope(self) -> spack.config.InternalConfigScope:

lib/spack/spack/cmd/bootstrap.py
@@ -436,6 +436,7 @@ def _mirror(args):
         shutil.copy(spack.util.path.canonicalize_path(GNUPG_JSON), abs_directory)
         shutil.copy(spack.util.path.canonicalize_path(PATCHELF_JSON), abs_directory)
         instructions += cmd.format("local-binaries", rel_directory)
+        instructions += "  % spack buildcache update-index <final-path>/bootstrap_cache\n"
     print(instructions)
 
 

lib/spack/spack/util/url.py
@@ -21,7 +21,7 @@ def validate_scheme(scheme):
     helps mostly in validation of paths vs urls, as Windows paths such as
     C:/x/y/z (with backward not forward slash) may parse as a URL with scheme
     C and path /x/y/z."""
-    return scheme in ("file", "http", "https", "ftp", "s3", "gs", "ssh", "git")
+    return scheme in ("file", "http", "https", "ftp", "s3", "gs", "ssh", "git", "oci")
 
 
 def local_file_path(url):